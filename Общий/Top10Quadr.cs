db1c7fde0a946f299ff3da6407a4d2d5ccd119c0
Helium Inert <helium.inert@outlook.com>
2021-04-29 13:50:39 +0300

    добавила класс с методом, выводящий квадраты первых 10 натуральных чисел

---
+++ Общий/Top10Quadr.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Общий
+{
+    class Top10Quadr
+    {
+        int[] quadr = new int[10];
+        public Top10Quadr()
+        {
+            for(int i = 1; i < 11; i++)
+            {
+                quadr[i - 1] = i * i;
+            }
+        }
+        public void ShowTop10Quadr()
+        {
+            foreach(var v in quadr)
+            {
+                Console.WriteLine(v);
+            }
+        }
+    }
+}
